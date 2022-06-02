fa6fac02a960b26121a310bc722ea67b421b36b2
Mussawer.Raza-at-101380220521 <mussawer@gosaas.io>
2022-06-02 22:35:44 +0500

    server setup db setup fix authorization middleware

--- server/db.js
+++ server/db.js
@@ -1,13 +1,28 @@
-const { Client } = require('pg')
-const client = new Client({
+const Pool = require("pg").Pool;
+require("dotenv").config();
+
+const devConfig = {
   user: 'postgres',
   host: 'localhost',
   database: 'postgres',
   password: '',
   port: 5433,
-})
+}
 
-client.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+const proConfig = {
+  connectionString: process.env.DATABASE_URL, //heroku addons
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
+
+
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
 });
+
+module.exports = pool;
---
+++ server/server.js
@@ -0,0 +1,38 @@
+const cors = require('cors');
+const express = require('express')
+require('dotenv').config();
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json()); // middleware to acces req.body
+app.use(express.urlencoded({ extended: false }));
+
+//middleware to handle any CORS issues
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, token"
+  );
+  if (req.method === "options") {
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use(routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`API Server now listening on port ${PORT}`);
+});
